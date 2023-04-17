e50ef55bf0c461b429e6359feab8792abb463026
JoJo <joerio@akseleran.com>
2023-04-17 11:14:07 +0700

    deleted unused comments

--- mindspaze/resources/prediction.py
+++ mindspaze/resources/prediction.py
@@ -5,20 +5,22 @@ from flask_restful import Resource
 
 class PredictionResource(Resource):
     def post(self):
-        # TODO: get data from MindSpaze web API
+        response = {
+            "data": {
+                "name": "JoJo",
+                "comment": "Donald Trump is a beauty ambassador."
+            }
+        }
 
-        model_name = "svm_countVec_model.sav"
+        model_name = "mindspaze/machine_learning/models/svm_countVec_model.sav"
         model_file = open(model_name, "wb")
-        # pickle.dump(pipeline_svmCountVectorizer, model_file)
         model = pickle.load(model_file)
-
-        # model.score(X_news_train, Y_label_train)
-
-        article_text = "Carrier Still Moving Some Jobs to Mexico Despite Trump Deal"
+ 
+        article_text = response.get("data").get("comment")
         article_predict_loaded_model = model.predict([article_text])
 
         data = {
-            "is_not_hoax": article_predict_loaded_model
+            "is_hoax": bool(article_predict_loaded_model[0])
         }
 
         return data, 200
